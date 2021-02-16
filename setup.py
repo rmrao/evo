f4db1c9faafc32f718ba46fb913932f5e34a7c80
Roshan Rao <roshan_rao@berkeley.edu>
2021-02-16 10:38:28 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+
+def get_version():
+    directory = os.path.abspath(os.path.dirname(__file__))
+    init_file = os.path.join(directory, "evo", "__init__.py")
+    with open(init_file) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
+
+
+with open("README.md", "r") as rf:
+    README = rf.read()
+
+with open("LICENSE", "r") as lf:
+    LICENSE = lf.read()
+
+with open("requirements.txt", "r") as reqs:
+    requirements = reqs.read().split()
+
+setup(
+    name="evo",
+    packages=find_packages(),
+    version=get_version(),
+    description="Mono-repository of protein utilities",
+    author="Roshan Rao",
+    author_email="roshan_rao@berkeley.edu",
+    url="https://github.com/rmrao/evo",
+    license=LICENSE,
+    keywords=["Proteins", "Deep Learning", "Pytorch"],
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+)

7b62371bde6dd5a03b634a9af9312733d344b24e
Roshan Rao <rmrao@berkeley.edu>
2021-02-19 13:28:31 -0500

    add contact precision metrics

---
+++ evo/metrics.py
@@ -0,0 +1,79 @@
+from typing import Optional
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+def compute_precisions(
+    predictions: torch.Tensor,
+    targets: torch.Tensor,
+    src_lengths: Optional[torch.Tensor] = None,
+    minsep: int = 6,
+    maxsep: Optional[int] = None,
+    override_length: Optional[int] = None,  # for casp
+):
+    if predictions.dim() == 2:
+        predictions = predictions.unsqueeze(0)
+    if targets.dim() == 2:
+        targets = targets.unsqueeze(0)
+
+    # Check sizes
+    if predictions.size() != targets.size():
+        raise ValueError(
+            f"Size mismatch. Received predictions of size {predictions.size()}, "
+            f"targets of size {targets.size()}"
+        )
+    device = predictions.device
+
+    batch_size, seqlen, _ = predictions.size()
+    seqlen_range = torch.arange(seqlen, device=device)
+
+    sep = seqlen_range.unsqueeze(0) - seqlen_range.unsqueeze(1)
+    sep = sep.unsqueeze(0)
+    valid_mask = sep >= minsep
+    valid_mask = valid_mask & (targets >= 0)  # negative targets are invalid
+
+    if maxsep is not None:
+        valid_mask &= sep < maxsep
+
+    if src_lengths is not None:
+        valid = seqlen_range.unsqueeze(0) < src_lengths.unsqueeze(1)
+        valid_mask &= valid.unsqueeze(1) & valid.unsqueeze(2)
+    else:
+        src_lengths = torch.full([batch_size], seqlen, device=device, dtype=torch.long)
+
+    predictions = predictions.masked_fill(~valid_mask, float("-inf"))
+
+    x_ind, y_ind = np.triu_indices(seqlen, minsep)
+    predictions_upper = predictions[:, x_ind, y_ind]
+    targets_upper = targets[:, x_ind, y_ind]
+
+    topk = seqlen if override_length is None else max(seqlen, override_length)
+    indices = predictions_upper.argsort(dim=-1, descending=True)[:, :topk]
+    topk_targets = targets_upper[torch.arange(batch_size).unsqueeze(1), indices]
+    if topk_targets.size(1) < topk:
+        topk_targets = F.pad(topk_targets, [0, topk - topk_targets.size(1)])
+
+    cumulative_dist = topk_targets.type_as(predictions).cumsum(-1)
+
+    gather_lengths = src_lengths.unsqueeze(1)
+    if override_length is not None:
+        gather_lengths = override_length * torch.ones_like(
+            gather_lengths, device=device
+        )
+
+    gather_indices = (
+        torch.arange(0.1, 1.1, 0.1, device=device).unsqueeze(0) * gather_lengths
+    ).type(torch.long) - 1
+
+    binned_cumulative_dist = cumulative_dist.gather(1, gather_indices)
+    binned_precisions = binned_cumulative_dist / (gather_indices + 1).type_as(
+        binned_cumulative_dist
+    )
+
+    pl5 = binned_precisions[:, 1]
+    pl2 = binned_precisions[:, 4]
+    pl = binned_precisions[:, 9]
+    auc = binned_precisions.mean(-1)
+
+    return {"AUC": auc, "P@L": pl, "P@L2": pl2, "P@L5": pl5}

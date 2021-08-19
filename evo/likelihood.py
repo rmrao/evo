b7fa40bc98863eef6b0652228f7e74b90196749c
Roshan Rao <rmrao@berkeley.edu>
2021-08-19 18:31:10 -0400

    add options to likelihood computation

--- evo/likelihood.py
+++ evo/likelihood.py
@@ -16,6 +16,7 @@ def sequence_logits(
     vocab: Vocab,
     sequence: str,
     verbose: bool = False,
+    mask_positions: bool = True,
     max_tokens: int = 2 ** 14,
 ) -> torch.Tensor:
 
@@ -24,21 +25,26 @@ def sequence_logits(
     tokens = torch.from_numpy(vocab.encode(sequence)).to(device)
     start = int(vocab.prepend_bos)
     end = tokens.size(-1) - int(vocab.append_eos)
-    tokens = tokens.unsqueeze(0).repeat(end - start, 1)
-    tokens[torch.arange(end - start), torch.arange(start, end)] = vocab.mask_idx
+    if mask_positions:
+        tokens = tokens.unsqueeze(0).repeat(end - start, 1)
+        tokens[torch.arange(end - start), torch.arange(start, end)] = vocab.mask_idx
 
-    logits = torch.zeros((end - start, len(vocab)), device=device)
-    batch_size = max_tokens // tokens.size(-1)
-    for i, batch in enumerate(
-        batched_iterator(tokens, batch_size=batch_size, device=device, verbose=verbose)
-    ):
-        idx = i * batch_size
+        logits = torch.zeros((end - start, len(vocab)), device=device)
+        batch_size = max_tokens // tokens.size(-1)
+        for i, batch in enumerate(
+            batched_iterator(
+                tokens, batch_size=batch_size, device=device, verbose=verbose
+            )
+        ):
+            idx = i * batch_size
 
-        batch_indices = torch.arange(batch.size(0))
-        logits[idx : idx + batch_size] = model(batch)["logits"][
-            batch_indices,
-            batch_indices + start + idx,
-        ]
+            batch_indices = torch.arange(batch.size(0))
+            logits[idx : idx + batch_size] = model(batch)["logits"][
+                batch_indices,
+                batch_indices + start + idx,
+            ]
+    else:
+        logits = model(tokens.unsqueeze(0))["logits"][0, start:end]
     return logits
 
 
@@ -47,8 +53,11 @@ def sequence_pseudo_ppl(
     model: esm.model.ProteinBertModel,
     vocab: Vocab,
     sequence: str,
+    mask_positions: bool = True,
     verbose: bool = False,
     max_tokens: int = 2 ** 14,
+    reduction: str = "mean",
+    log: bool = False,
 ) -> float:
 
     device = next(model.parameters()).device
@@ -58,12 +67,19 @@ def sequence_pseudo_ppl(
     residue_tokens = tokens[start:end]
 
     logits = sequence_logits(
-        model, vocab, sequence, verbose=verbose, max_tokens=max_tokens
+        model,
+        vocab,
+        sequence,
+        mask_positions=mask_positions,
+        verbose=verbose,
+        max_tokens=max_tokens,
     )
 
-    pseudo_ppl = nn.CrossEntropyLoss()(
+    pseudo_ppl = nn.CrossEntropyLoss(reduction=reduction)(
         logits.view(-1, len(vocab)), residue_tokens
-    ).exp()
+    )
+    if not log:
+        pseudo_ppl = pseudo_ppl.exp()
     return pseudo_ppl.item()
 
 
@@ -72,7 +88,9 @@ def pseudo_ppl(
     model: esm.model.ProteinBertModel,
     alphabet_or_vocab: Union[esm.data.Alphabet, Vocab],
     sequences: List[str],
+    mask_positions: bool = True,
     max_tokens: int = 2 ** 14,
+    log: bool = False,
 ):
     if not isinstance(alphabet_or_vocab, Vocab):
         vocab = Vocab.from_esm_alphabet(alphabet_or_vocab)
@@ -81,7 +99,14 @@ def pseudo_ppl(
 
     model = model.cuda().eval()
 
-    compute = partial(sequence_pseudo_ppl, model, vocab, max_tokens=max_tokens)
+    compute = partial(
+        sequence_pseudo_ppl,
+        model,
+        vocab,
+        max_tokens=max_tokens,
+        mask_positions=mask_positions,
+        log=log,
+    )
 
     pseudo_ppl = []
     for sequence in tqdm(sequences):

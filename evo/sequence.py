9d585fecee14c8250bb1c70d9f3f7f5a889462b3
Roshan Rao <rmrao@berkeley.edu>
2021-08-19 18:25:07 -0400

    fix issue with mutant splits

--- evo/sequence.py
+++ evo/sequence.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import re
 from itertools import product
 import pandas as pd
 
@@ -19,16 +20,20 @@ def mutant_to_names(mutant: str) -> Tuple[str, int, str]:
     return mutant[0], int(mutant[1:-1]), mutant[-1]
 
 
-def make_mutation(sequence: str, mutant: str) -> str:
-    if "," in mutant:
-        mutants = ",".split(mutant)
+def make_mutation(sequence: str, mutant: str, start_ind: int = 1) -> str:
+    delimiters = [",", r"\+", ":"]
+    expression = re.compile("|".join(delimiters))
+    if mutant.upper() == "WT":
+        return sequence
+    if expression.search(mutant):
+        mutants = expression.split(mutant)
         for mutant in mutants:
             sequence = make_mutation(sequence, mutant)
         return sequence
     else:
         wt, pos, mut = mutant_to_names(mutant)
-        assert sequence[pos - 1] == wt
-        return sequence[:pos - 1] + mut + sequence[pos:]
+        assert sequence[pos - start_ind] == wt
+        return sequence[:pos - start_ind] + mut + sequence[pos - start_ind + 1:]
 
 
 def create_mutant_df(sequence: str) -> pd.DataFrame:
